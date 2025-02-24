5dcbb0bf2ef2bddc7bec64826d8b9885598b6d29
Victor <victor.ched@gmail.com>
2025-02-24 00:10:23 -0500

    add ascii to svg generator

---
+++ ascii_to_svg.py
@@ -0,0 +1,19 @@
+MAIN_X = 400
+STARTING_Y = 30
+
+
+def main():
+    with open("ascii.txt", "r") as f:
+        lines = f.readlines()
+        new_lines = []
+        for i, l in enumerate(lines):
+            formatting_front = f'<tspan x="{MAIN_X}" y="{STARTING_Y + 20 * (i)}">'
+            formatting_back = "</tspan>"
+            new_lines.append(formatting_front + l.rstrip() + formatting_back)
+
+    with open("ascii.svg", "w") as f:
+        f.write("\n".join(new_lines))
+
+
+if __name__ == "__main__":
+    main()
